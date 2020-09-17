4aa37ad885cdaa1011f132025e0db955e5e3e788
Kevin <59708179+KevinMims96@users.noreply.github.com>
2020-09-16 23:00:23 -0400

    1.3 Update

--- jquery.js
+++ jquery.js
@@ -2,4 +2,16 @@ $(document).ready(() => {
   $(".drop-btn").on("click", () => {
       $(".dropdown-content").slideToggle(250)
   })
+
+  $(".first-d-dropdown").on("click", () => {
+      $(".first-description").slideToggle()
+  })
+
+  $(".second-d-dropdown").on("click", () => {
+    $(".second-description").slideToggle()
+})
+
+$(".third-d-dropdown").on("click", () => {
+    $(".third-description").slideToggle()
+})
 })
